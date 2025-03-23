3e8b242ea10cbd68265d3a89d9257c72380dc79e
MJunKWON <empal44@naver.com>
2025-03-24 02:11:16 +0900

    Add App component

---
+++ packages/frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>MultiAPI Project</h1>
+      <p>Welcome to the MultiAPI Project</p>
+    </div>
+  )
+}
+
+export default App
