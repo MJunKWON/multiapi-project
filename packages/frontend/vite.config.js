394e03d93a467091ffef0f4165aa479d20b5ee8b
MJunKWON <empal44@naver.com>
2025-03-24 02:39:47 +0900

    Optimize Vite configuration

--- packages/frontend/vite.config.js
+++ packages/frontend/vite.config.js
@@ -1,11 +1,24 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
+  root: path.resolve(__dirname, ''),
   build: {
     outDir: 'dist',
+    emptyOutDir: true,
     sourcemap: true,
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, 'index.html'),
+      },
+    },
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
   },
   server: {
     host: true,
