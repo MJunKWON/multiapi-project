a4f9c5997d9b7dc94a0ba9b44b687a2e641ce8ca
MJunKWON <empal44@naver.com>
2025-03-24 14:40:41 +0900

    Remove experimental.appDir config

--- packages/frontend/next.config.js
+++ packages/frontend/next.config.js
@@ -3,9 +3,6 @@ const nextConfig = {
   output: 'standalone',
   reactStrictMode: true,
   swcMinify: true,
-  experimental: {
-    appDir: true
-  },
   webpack: (config) => {
     config.resolve.fallback = { fs: false, net: false, tls: false };
     return config;
