9868fa65d61c422c1f29aae1809084fd1a9e40e2
MJunKWON <empal44@naver.com>
2025-03-24 13:28:22 +0900

    Update Next.js configuration

--- packages/frontend/next.config.js
+++ packages/frontend/next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true
+  output: 'standalone',
+  reactStrictMode: true,
+  swcMinify: true,
+  experimental: {
+    appDir: true
+  },
+  webpack: (config) => {
+    config.resolve.fallback = { fs: false, net: false, tls: false };
+    return config;
+  }
 }
 
 module.exports = nextConfig
