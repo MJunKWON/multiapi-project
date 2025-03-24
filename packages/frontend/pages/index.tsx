49b6d979f7fdedfea3db55d3c5808b3b23f5bf72
MJunKWON <empal44@naver.com>
2025-03-24 13:06:19 +0900

    Simplify Next.js project structure

---
+++ packages/frontend/pages/index.tsx
@@ -0,0 +1,13 @@
+import { Box, Container, Heading } from '@chakra-ui/react'
+
+export default function Home() {
+  return (
+    <Container maxW="container.xl" py={8}>
+      <Box textAlign="center">
+        <Heading as="h1" size="2xl" mb={8}>
+          MultiAPI AI Chatbot
+        </Heading>
+      </Box>
+    </Container>
+  )
+}
